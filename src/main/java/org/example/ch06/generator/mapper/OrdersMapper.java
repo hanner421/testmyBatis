8b8f748a02c08eb4fd5bd2847c6327ed434644bf
Yhanner421 <515388539@qq.com>
2020-07-15 14:38:11 +0800

    高级映射中多对多的相关操作以及逆向工程

---
+++ src/main/java/org/example/ch06/generator/mapper/OrdersMapper.java
@@ -0,0 +1,30 @@
+package org.example.ch06.generator.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.example.ch06.generator.pojo.Orders;
+import org.example.ch06.generator.pojo.OrdersExample;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
